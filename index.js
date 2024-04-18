1553dc6c74ae28b1df90d2d68bb4f02a83b92705
Maaz Husyan <159529283+MaazHusyan@users.noreply.github.com>
2024-04-19 04:14:30 +0500

    Typescript Words Counter

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let condition = true;
+do {
+    let answer = await inquirer.prompt([
+        {
+            name: "sentence",
+            message: "Enter your para to count the words: => ",
+            type: "input",
+        },
+    ]);
+    let countWords = answer.sentence.trim().split(" ");
+    console.log(countWords.length);
+} while (condition);
